1b3653bc507643584e3d76e08906d324aa0517be
zhernovkov <zhernovkov@alfasystem.ru>
2022-01-03 13:49:12 +0300

    initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"go-stripe/internal/driver"
+	"go-stripe/internal/models"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+	}
+	secretKey string
+	frontend  string
+}
+
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	version  string
+	DB       models.DBModel
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Println(fmt.Sprintf("Starting backend server in %s on port %d", app.config.env, app.config.port))
+
+	return srv.ListenAndServe()
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production|maintenance}")
+	flag.StringVar(&cfg.db.dsn, "dsn", "", "DSN")
+	flag.StringVar(&cfg.smtp.host, "smtphost", "", "smtp host")
+	flag.IntVar(&cfg.smtp.port, "smtpport", 587, "smtp port")
+	flag.StringVar(&cfg.smtp.username, "smtpuser", "", "smtp username")
+	flag.StringVar(&cfg.smtp.password, "smtppass", "", "smtp password")
+	flag.StringVar(&cfg.secretKey, "secret", "", "secret key")
+	flag.StringVar(&cfg.frontend, "frontend", "http://localhost:4000", "url to frontend")
+
+	flag.Parse()
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	conn, err := driver.OpenDB(cfg.db.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer func(conn *sql.DB) {
+		err := conn.Close()
+		if err != nil {
+			errorLog.Fatal(err)
+		}
+	}(conn)
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		version:  version,
+		DB:       models.DBModel{DB: conn},
+	}
+
+	err = app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
